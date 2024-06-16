6c25418b418804fc3be17027d771833fe897470d
Martux1995 <bastianruizgaray@gmail.com>
2024-06-16 03:44:44 -0400

    :sparkles: Implemented Session Repository

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,11 +1,17 @@
-import { Module, Provider } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module, Provider } from '@nestjs/common';
 
 import { ConfigModule } from '../config/config.module';
 import { ConfigService } from '../config/config.service';
+import { getTypeOrmModuleOptions } from './typeorm.config';
 
 import * as ENTITIES from './entities';
-import { getTypeOrmModuleOptions } from './typeorm.config';
+import { SessionRepositoryImp } from './repositories';
+import { SESSION_REPOSITORY } from '../domain/repositories';
+
+const REPOSITORIES: Provider[] = [
+  { provide: SESSION_REPOSITORY, useClass: SessionRepositoryImp },
+];
 
 @Module({
   imports: [
@@ -16,5 +22,7 @@ import { getTypeOrmModuleOptions } from './typeorm.config';
     }),
     TypeOrmModule.forFeature(Object.values(ENTITIES)),
   ],
+  providers: [...REPOSITORIES],
+  exports: [...REPOSITORIES],
 })
 export class DatabaseModule {}
