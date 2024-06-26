f913c34ce067355c1f19c7822bb0390c24e2c87e
Martux1995 <bastianruizgaray@gmail.com>
2024-06-26 03:19:25 -0400

    :sparkles: Added auth repository for login purposes

--- src/database/repositories/index.ts
+++ src/database/repositories/index.ts
@@ -1,10 +1,14 @@
 import { Provider } from '@nestjs/common';
 
-import { SCORE_REPOSITORY } from '../../domain/repositories';
+import { AuthRepositoryImp } from './auth.repository';
 import { ScoreRepositoryImp } from './score.repository';
 
+import { AUTH_REPOSITORY, SCORE_REPOSITORY } from '../../domain/repositories';
+
 export * from './score.repository';
+export * from './auth.repository';
 
 export const REPOSITORIES: Provider[] = [
   { provide: SCORE_REPOSITORY, useClass: ScoreRepositoryImp },
+  { provide: AUTH_REPOSITORY, useClass: AuthRepositoryImp },
 ];
