856988954e5b12124b2d79e72814eb63b072961b
Martux1995 <bastianruizgaray@gmail.com>
2025-06-01 04:15:35 -0400

    :recycle: Changed fixed db for selector db between postgres or sqlite

--- src/database/datasource.config.ts
+++ src/database/datasource.config.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { ConfigService } from '../config/config.service';
 import { ConfigModule } from '../config/config.module';
+import { dbSelector } from './typeorm.config';
 
 export default (async () => {
   const app = await NestFactory.create(ConfigModule, {
@@ -12,8 +13,7 @@ export default (async () => {
   const config = app.get(ConfigService);
 
   return new DataSource({
-    type: 'postgres',
-    url: config.getDatabaseUrl(),
+    ...dbSelector(config),
     entities: ['src/database/entities/*.entity{.js,.ts}'],
     migrations: ['src/database/migrations/*{.js,.ts}'],
     synchronize: false,
