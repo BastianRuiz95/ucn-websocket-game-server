607e16966bfff0b1eb4af61f5ab84588f53c5c7c
Martux1995 <bastianruizgaray@gmail.com>
2025-06-02 22:46:25 -0400

    :recycle: fixed infrastructure broken paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { HttpModule } from './http/http.module';
-import { ConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
 import { WebsocketModule } from './websocket/websocket.module';
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, HttpModule, WebsocketModule],
+  imports: [HttpModule, WebsocketModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { ConfigService } from './config/config.service';
+import { ConfigService } from './infrastructure/config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
