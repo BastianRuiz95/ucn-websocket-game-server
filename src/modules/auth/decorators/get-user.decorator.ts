0cc0347a6dce2cc8a8388dc40b784c73d068f9c8
Martux1995 <bastianruizgaray@gmail.com>
2025-06-03 02:14:55 -0400

    :art: Fixed Major ESlint warnings & errors

--- src/modules/auth/decorators/get-user.decorator.ts
+++ src/modules/auth/decorators/get-user.decorator.ts
@@ -3,9 +3,10 @@ import {
   InternalServerErrorException,
   createParamDecorator,
 } from '@nestjs/common';
+import { UserRequest } from '../interfaces';
 
 export const getUserFromRequestFn = (_params: any, ctx: ExecutionContext) => {
-  const user = ctx.switchToHttp().getRequest().user;
+  const user = ctx.switchToHttp().getRequest<{ user: UserRequest }>().user;
 
   if (!user) {
     throw new InternalServerErrorException('User not found (Decorator error)');
