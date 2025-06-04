a230e2be4b6cc6944426a8d44d29c14e84570cd7
Martux1995 <bastianruizgaray@gmail.com>
2025-06-04 02:17:47 -0400

    :recycle: Changed ws implementation

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { SocketService } from './services/socket.service';
-import { ConnectionGateway, LobbyGateway } from './gateways';
+
+import { LobbyModule } from './lobby/lobby.module';
+import { PlayerModule } from './player/player.module';
+import { PlayerListModule } from './player-list/player-list.module';
+import { MatchmakingModule } from './matchmaking/matchmaking.module';
+
+import { ConnectionHandlerGateway } from './config/connection-handler.gateway';
 
 @Module({
-  providers: [SocketService, ConnectionGateway, LobbyGateway],
+  imports: [PlayerModule, PlayerListModule, LobbyModule, MatchmakingModule],
+  providers: [ConnectionHandlerGateway],
 })
 export class WebsocketModule {}
