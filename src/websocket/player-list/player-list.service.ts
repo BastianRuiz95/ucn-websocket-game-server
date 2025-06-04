6ebfbc0e0a405915d0423d34eaf9f0526680a74a
Martux1995 <bastianruizgaray@gmail.com>
2025-06-04 01:56:14 -0400

    :sparkles: Added player list module

---
+++ src/websocket/player-list/player-list.service.ts
@@ -0,0 +1,37 @@
+import { WebSocket } from 'ws';
+import { Injectable } from '@nestjs/common';
+
+import { Player } from '../player/player';
+
+@Injectable()
+export class PlayerListService {
+  private readonly _playerList: Player[] = [];
+
+  addPlayer(player: Player) {
+    this._playerList.push(player);
+  }
+
+  removePlayer(player: Player) {
+    this._playerList.splice(this._playerList.indexOf(player), 1);
+  }
+
+  getPlayers(): Player[] {
+    return this._playerList;
+  }
+
+  getPlayerBySocket(socketClient: WebSocket): Player {
+    return (
+      this._playerList.find((p) => p.getSocketClient() === socketClient) || null
+    );
+  }
+
+  getPlayerById(playerId: string): Player {
+    return this._playerList.find((p) => p.getId() === playerId) || null;
+  }
+
+  broadcast<T = object>(event: string, data: T, omitPlayerId: string = null) {
+    this._playerList.forEach((p) => {
+      if (!omitPlayerId || omitPlayerId != p.getId()) p.sendEvent(event, data);
+    });
+  }
+}
