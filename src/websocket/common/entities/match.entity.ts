0716d7d879271b8ba99d053385ed4a420894c272
Martux1995 <bastianruizgaray@gmail.com>
2025-06-08 03:35:18 -0400

    :recycle: Changed match entity class logic to match ts standars

--- src/websocket/common/entities/match.entity.ts
+++ src/websocket/common/entities/match.entity.ts
@@ -1,36 +1,17 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { Player } from './player.entity';
 import { EMatchStatus } from '../enums';
-import { PlayerPresenter } from '../presenters';
 
 export class Match {
-  private readonly id: string;
-  constructor(
-    private status: EMatchStatus,
-    private readonly playerOne: Player,
-    private readonly playerTwo: Player,
-  ) {
-    this.id = uuidv4();
+  readonly id: string;
+  readonly playerOne: Player;
+  readonly playerTwo: Player;
+  status: EMatchStatus;
+
+  constructor(partial: Partial<Match>) {
+    Object.assign(this, partial);
   }
 
-  getId(): string {
-    return this.id;
-  }
-
-  getStatus(): EMatchStatus {
-    return this.status;
-  }
-
-  setStatus(status: EMatchStatus): void {
-    this.status = status;
-  }
-
-  getPlayers(): Player[] {
-    return [this.playerOne, this.playerTwo];
-  }
-
-  getPlayersData(): PlayerPresenter[] {
+  getPlayers() {
     return [this.playerOne.getPlayerData(), this.playerTwo.getPlayerData()];
   }
 }
