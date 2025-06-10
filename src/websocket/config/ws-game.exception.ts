29d32b298e2decdb46ff346fec8d43bcb16a7e55
Martux1995 <bastianruizgaray@gmail.com>
2025-06-09 22:28:31 -0400

    :sparkles: Added static function for cyclomatic complex reduction

--- src/websocket/config/ws-game.exception.ts
+++ src/websocket/config/ws-game.exception.ts
@@ -1,6 +1,10 @@
 import { WsException } from '@nestjs/websockets';
 
 export class WsGameException extends WsException {
+  static throwException(message: string, data: object) {
+    throw new WsGameException(message, data);
+  }
+
   constructor(
     public gameMessage: string,
     public data: object,
