6a62f7b5b59d7087e50a44b675a5860ba19cd4e0
Martux1995 <bastianruizgaray@gmail.com>
2025-06-09 22:27:37 -0400

    :sparkles: Added exception filter to lobby functions and exceptions cases

--- src/websocket/lobby/lobby.service.ts
+++ src/websocket/lobby/lobby.service.ts
@@ -4,6 +4,7 @@ import { Player } from '../common/entities';
 
 import { ELobbyEvent } from './lobby-event.enum';
 import { PlayerListService } from '../player-list/player-list.service';
+import { WsGameException } from '../config/ws-game.exception';
 
 @Injectable()
 export class LobbyService {
@@ -20,36 +21,49 @@ export class LobbyService {
   ) {
     const playerToSendMsg = this.playerListService.getPlayerById(playerId);
     if (!playerToSendMsg) {
-      // Throw Error(`Player '${playerId}' not exists`)
-      return;
+      WsGameException.throwException(`Player with ID ${playerId} not exists.`, {
+        playerId,
+      });
     }
 
+    this._checkMessage(playerMsg);
+
     playerToSendMsg.sendEvent(ELobbyEvent.PrivateMessageReceived, {
-      msg: `Player '${senderPlayer.name}' sended you a private message`,
+      msg: `Player '${senderPlayer.name}' have sent you a private message`,
       playerId: senderPlayer.id,
       playerName: senderPlayer.name,
-      playerMsg,
+      playerMsg: playerMsg.trim(),
     });
 
     return {
-      msg: `Message sended to ${playerToSendMsg.name}`,
+      msg: `Message sent to ${playerToSendMsg.name}`,
     };
   }
 
   sendPublicMessage(senderPlayer: Player, playerMsg: string) {
+    this._checkMessage(playerMsg);
+
     this.playerListService.broadcast(
       ELobbyEvent.PublicMessageReceived,
       {
-        msg: `Player '${senderPlayer.name}' sended a message`,
+        msg: `Player '${senderPlayer.name}' have sent a message`,
         playerId: senderPlayer.id,
         playerName: senderPlayer.name,
-        playerMsg,
+        playerMsg: playerMsg.trim(),
       },
       senderPlayer.id,
     );
 
     return {
-      msg: 'Message sended to all players',
+      msg: 'Message sent to all players',
     };
+  }
+
+  private _checkMessage(message: string) {
+    if (!message || message.trim().length === 0) {
+      WsGameException.throwException(`You cannot send an empty message`, {
+        message: message ?? typeof message,
+      });
+    }
   }
 }
