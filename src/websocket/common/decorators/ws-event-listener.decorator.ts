c7f2b1f24dc34df29c205a41c7080613f274871c
Martux1995 <bastianruizgaray@gmail.com>
2025-06-12 01:02:47 -0400

    :sparkles: Added interceptor to the WsEventListener decorator

--- src/websocket/common/decorators/ws-event-listener.decorator.ts
+++ src/websocket/common/decorators/ws-event-listener.decorator.ts
@@ -1,11 +1,13 @@
 import { SubscribeMessage } from '@nestjs/websockets';
-import { applyDecorators, UseFilters } from '@nestjs/common';
+import { applyDecorators, UseFilters, UseInterceptors } from '@nestjs/common';
 
-import { WsGameExceptionFilter } from '../filters/ws-game-exception.filter';
+import { WsGameExceptionFilter } from '../filters';
+import { WsParseResultInterceptor } from '../interceptors';
 
 export function WsEventListener(event: string) {
   return applyDecorators(
     UseFilters(new WsGameExceptionFilter(event)),
+    UseInterceptors(new WsParseResultInterceptor(event)),
     SubscribeMessage(event),
   );
 }
