214c2f198fec1940c389df6ebca85cb08950906f
Martux1995 <bastianruizgaray@gmail.com>
2025-06-12 23:48:07 -0400

    :recycle: renamed enum

--- src/websocket/common/enums/index.ts
+++ src/websocket/common/enums/index.ts
@@ -4,4 +4,4 @@ export * from './player-status.enum';
 export * from './lobby-trigger-event.enum';
 export * from './player-trigger-event.enum';
 export * from './game-match-trigger-event.enum';
-export * from './matchmaking-trigger-event';
+export * from './matchmaking-trigger-event.enum';
