d87a85301ff3d733f80d7d251b2a2ea72083efde
Bastian Ruiz <bastian.ruiz@ce.ucn.cl>
2025-07-02 17:07:20 -0400

    :recycle: Basis for list refactor

--- src/websocket/player-list/player-list.service.ts
+++ src/websocket/player-list/player-list.service.ts
@@ -6,28 +6,39 @@ import { EPlayerStatus } from '../common/enums';
 
 @Injectable()
 export class PlayerListService {
-  private readonly _playerList: Player[] = [];
+  // private readonly _playerList: Player[] = [];
+  private readonly _playerList: Record<string, Player> = {};
 
   addPlayer(player: Player) {
-    this._playerList.push(player);
+    this._playerList[player.id] = player;
   }
 
   removePlayer(player: Player) {
-    this._playerList.splice(this._playerList.indexOf(player), 1);
+    delete this._playerList[player.id];
   }
 
   getPlayers(): Player[] {
-    return this._playerList.filter((p) => p.status !== EPlayerStatus.NoLogin);
+    return Object.keys(this._playerList).map((v) => {
+      return (
+        this._playerList[v] &&
+        this._playerList[v].status !== EPlayerStatus.NoLogin &&
+        this._playerList[v].status !== EPlayerStatus.Disconnected &&
+        this._playerList[v]
+      );
+    });
   }
 
+  // TODO: Refactor this to O(1) complex
   getPlayerBySocket(socketClient: WebSocket): Player {
-    return (
-      this._playerList.find((p) => p.socketClient === socketClient) || null
-    );
+    return Object.keys(this._playerList)
+      .map((v) => {
+        return this._playerList[v];
+      })
+      .find((p) => p.socketClient == socketClient);
   }
 
   getPlayerById(playerId: string): Player {
-    return this.getPlayers().find((p) => p.id === playerId) || null;
+    return this._playerList[playerId] || null;
   }
 
   broadcast<T = object>(
